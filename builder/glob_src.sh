1026511364a17f0b1e1b59c1423eda3e9c6b715b
liyunlai <liyunlai@jingxunsoft.com>
2024-11-27 10:22:34 +0800

    update scripts

--- builder/glob_src.sh
+++ builder/glob_src.sh
@@ -152,7 +152,7 @@ EOF
 
 -include $(realpath -m --relative-to=$(dirname ${ENTRY_MK}) ${INIT_MK})
 
-COM_COMPILE_FLAGS += -Os
+# COM_COMPILE_FLAGS += -Os
 COM_COMPILE_FLAGS += -fPIC
 COM_COMPILE_FLAGS += -fdata-sections -ffunction-sections
 COM_COMPILE_FLAGS += -fno-omit-frame-pointer
@@ -171,6 +171,14 @@ TARGET = ${TARGET_NAME}
 all: dump_compile_info \${TARGET}
 
 -include $(realpath -m --relative-to=$(dirname ${ENTRY_MK}) ${DEFS_MK})
+
+# \$(call rm_duplicate,CFLAGS)
+# \$(call rm_duplicate,CXXFLAGS)
+# \$(call rm_duplicate,INCLUDES)
+# \$(call rm_duplicate,CPPFLAGS)
+# \$(call rm_duplicate,LDFLAGS)
+# \$(call rm_duplicate,LIBS)
+
 include $(basename ${SRC_MK})
 
 \${TARGET}: \${OBJS}
--- builder/makefile.defs.mk
+++ builder/makefile.defs.mk
@@ -1,16 +1,36 @@
 
+# standard
+CFLAGS += -std=gnu11
+CXXFLAGS += -std=gnu++14
+
+# optimize level
+CFLAGS += -O0 -g
+CXXFLAGS += -O0 -g
+
+
 # address sanitizer
 CFLAGS += -fsanitize=address
 CXXFLAGS += -fsanitize=address
+
+# LDFLAGS += -Wl,--as-needed
+# LDFLAGS += -Wl,--allow-shlib-undefined
+
 LIBS += -lasan
 
-# 
-LIBS += -lstdc++
-LIBS += -lpthread
-
-# add openssl
-# $(call pkgconf_add, openssl)
+# pkgconf --list-all | awk '{print $1}' | grep opencv
+# $(call pkgconf_add, opencv4)
+# $(call pkgconf_add, protobuf)
+# $(call pkgconf_add, glew)
 
 # add libcurl
 # $(call pkgconf_add, libcurl)
 
+# add openssl
+# $(call pkgconf_add, openssl)
+
+# libqrencode
+# $(call pkgconf_add, libqrencode)
+
+# 
+LIBS += -lstdc++
+LIBS += -lpthread
