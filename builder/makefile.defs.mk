c6fc2474423c5db18da89c970c42a9a44f7f0987
liyunlai <liyunlai@jingxunsoft.com>
2025-03-10 18:41:17 +0800

    nothing

--- builder/makefile.defs.mk
+++ builder/makefile.defs.mk
@@ -9,13 +9,13 @@ $(call get_relative_dir, rpathd)
 # -include ${rpathd}/lib_nanogui.mk
 # -include ${rpathd}/lib_glew.mk
 
--include ${rpathd}/lib_opencv4.mk
+# -include ${rpathd}/lib_opencv4.mk
 # -include ${rpathd}/lib_protobuf.mk
--include ${rpathd}/lib_qrencode.mk
--include ${rpathd}/lib_curl.mk
--include ${rpathd}/lib_openssl.mk
+# -include ${rpathd}/lib_qrencode.mk
+# -include ${rpathd}/lib_curl.mk
+# -include ${rpathd}/lib_openssl.mk
 
--include ${rpathd}/lib_alsa.mk
--include ${rpathd}/lib_sqlite3.mk
+# -include ${rpathd}/lib_alsa.mk
+# -include ${rpathd}/lib_sqlite3.mk
 
 -include ${rpathd}/compile_flags_end.mk
